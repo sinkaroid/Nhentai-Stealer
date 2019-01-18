eed4700f63c514ff2d4e2d4578e67f418908fee1
sinkaroid <bibsindra@gmail.com>
2019-01-19 03:13:23 +0700

    Create mirror.php

---
+++ mirror.php
@@ -0,0 +1,33 @@
+<title>nHentai Stealer mirror</title>
+<link rel="shortcut icon" href="https://4.bp.blogspot.com/-DweioerHPNg/XEAf4rEqqqI/AAAAAAAABLc/d_BTxB3uqro1gxaU-iSgK0Apu3YA7pMfgCLcBGAs/s1600/slemanb0ys-ico.jpg" type="image/x-icon">
+<meta name='author' content='Sinkaroid Stupidc0de'>
+<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css">
+<center><font face="Ubuntu" size="5" color="black"><b>[!] STEALING SUCCEED [!]<b/></font></center>
+<br>
+
+<center><font face="Ubuntu" size="4" color="black"><b><i>a sec till complete.. then, go put in nhentai connect</i><b/></font></center>
+<center><font face="Ubuntu" size="2" color="black"><b><i>u can touch this kato face</i><b/></font></center>
+<center><a href="connect.php" rel="nofollow" target="_blank"><img src="https://lh3.googleusercontent.com/-EvJVjsdiFDg/XEAXkLbZpGI/AAAAAAAABKw/wOPDsCIg4vsB14vljDh7C8YgrzwUjb7uACLcBGAs/h120/kano.png" width="100" height="100"/><a/><br /><center>
+<p>
+<center>
+<?php
+for ($i= 1; $i <= 24; $i++) ///IMPORTANT ,check the folder, see how many pictures there are,  change the variable [24] to max numb in nhentai.net gallery
+{
+echo" <img width='400' height='540' src=";
+/**geturl**/ echo "https://i.nhentai.net/galleries";
+echo "/1345174/"; ///put code here 
+echo $i; echo".jpg"; ///check nh folder first, if png format just change it
+echo ">";
+echo "";
+}
+
+
+for ($i= 1; $i <= 25; $i++)
+
+?>
+<p>
+<center><font face="Ubuntu" size="5" color="black"><b>[!] nHentai Stealer [!]<b/></font></center>
+<br>
+<center><font face="Ubuntu" size="3" color="black"><b><i>(c) 2019 coded by sinkaroid ~penjagabisu@outlook.com</i><b/></font></center>
+<a oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' style='display:scroll;position:fixed;Bottom:0px;right:0px;'><img width='370' height='530' src='https://4.bp.blogspot.com/-aW-gKT7Xufc/XD7hbr2lwmI/AAAAAAAABII/vV9qS6qKUuQgtHRZz11iVrO6yyVAGzI5ACLcBGAs/s1600/katousan.png'/></a>
+<a oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' style='display:scroll;position:fixed;Bottom:0px;left:0px;'><img width='300' height='500' src='https://4.bp.blogspot.com/-jg9b22SkotY/XEBOzm2gCzI/AAAAAAAABL0/RVZMLg9qZQssXG9TEAxiiUe1ChufD1TRQCLcBGAs/s1600/kasu.png'/></a>
